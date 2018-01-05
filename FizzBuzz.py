d2e226c03fc265f3e3e1888a2a7671d40264db86
Conor Kennedy <35005030+suelz@users.noreply.github.com>
2018-01-04 19:57:22 -0600

    optimize fizz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -5,9 +5,10 @@
 
 
 for x in range(1,101):
-    if(x % 3 ==0 and x % 5 == 0):
+    if(x%3 == 0):
+      if(x%5 == 0):
         print("FizzBuzz")
-    elif(x%3 == 0):
+      else:
         print("Fizz")
     elif(x%5 == 0):
         print("Buzz")
